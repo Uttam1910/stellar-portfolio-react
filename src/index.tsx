6fd03699846b8fbddea59250c36d67516260a859
Uttam1910 <uttamt1202@gmail.com>
2024-07-26 06:17:56 +0530

    Updated Sidebar component to use correct image path and fixed image alt attribute

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import emailjs from 'emailjs-com';
 
 emailjs.init('YOUR_USER_ID');
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container!);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
