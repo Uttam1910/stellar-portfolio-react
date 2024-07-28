cde77cfc885ebb06ffc29194becaff528d45e5e5
Uttam1910 <uttamt1202@gmail.com>
2024-07-28 21:14:38 +0530

    updated  sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -26,14 +26,14 @@ const Sidebar: React.FC = () => {
         </div>
         <nav className="flex-1">
           <ul className="flex flex-col space-y-1">
-            <li><Link to="/" className="flex items-center p-4 hover:bg-teal-700"><FaHome className="mr-3" /> Home</Link></li>
-            <li><Link to="/about" className="flex items-center p-4 hover:bg-teal-700"><FaUser className="mr-3" /> About</Link></li>
-            <li><Link to="/experience" className="flex items-center p-4 hover:bg-teal-700"><FaBriefcase className="mr-3" /> Experience</Link></li>
-            <li><Link to="/projects" className="flex items-center p-4 hover:bg-teal-700"><FaProjectDiagram className="mr-3" /> Projects</Link></li>
-            <li><Link to="/skills" className="flex items-center p-4 hover:bg-teal-700"><FaCode className="mr-3" /> Skills</Link></li>
-            <li><Link to="/education" className="flex items-center p-4 hover:bg-teal-700"><FaGraduationCap className="mr-3" /> Education</Link></li>
-            <li><Link to="/contact" className="flex items-center p-4 hover:bg-teal-700"><FaEnvelope className="mr-3" /> Contact</Link></li>
-            <li><a href="pdf/jobresume.pdf" target="_blank" rel="noopener noreferrer" className="flex items-center p-4 hover:bg-teal-700"><FaFileAlt className="mr-3" /> Resume</a></li>
+            <li><Link to="/" className="flex items-center p-4 hover:bg-teal-700" onClick={toggleSidebar}><FaHome className="mr-3" /> Home</Link></li>
+            <li><Link to="/about" className="flex items-center p-4 hover:bg-teal-700" onClick={toggleSidebar}><FaUser className="mr-3" /> About</Link></li>
+            <li><Link to="/experience" className="flex items-center p-4 hover:bg-teal-700" onClick={toggleSidebar}><FaBriefcase className="mr-3" /> Experience</Link></li>
+            <li><Link to="/projects" className="flex items-center p-4 hover:bg-teal-700" onClick={toggleSidebar}><FaProjectDiagram className="mr-3" /> Projects</Link></li>
+            <li><Link to="/skills" className="flex items-center p-4 hover:bg-teal-700" onClick={toggleSidebar}><FaCode className="mr-3" /> Skills</Link></li>
+            <li><Link to="/education" className="flex items-center p-4 hover:bg-teal-700" onClick={toggleSidebar}><FaGraduationCap className="mr-3" /> Education</Link></li>
+            <li><Link to="/contact" className="flex items-center p-4 hover:bg-teal-700" onClick={toggleSidebar}><FaEnvelope className="mr-3" /> Contact</Link></li>
+            <li><a href="pdf/jobresume.pdf" target="_blank" rel="noopener noreferrer" className="flex items-center p-4 hover:bg-teal-700" onClick={toggleSidebar}><FaFileAlt className="mr-3" /> Resume</a></li>
           </ul>
         </nav>
         <div className="p-4 flex justify-center space-x-4 bg-gray-900">
