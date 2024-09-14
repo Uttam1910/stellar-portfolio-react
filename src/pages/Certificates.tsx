8d7217a63886aa0d49d8ec793ab634ca0bb72265
Uttam1910 <uttamt1202@gmail.com>
2024-09-14 12:23:05 +0530

    added certificates

--- src/pages/Certificates.tsx
+++ src/pages/Certificates.tsx
@@ -1,42 +1,43 @@
 import React from 'react';
 import { FaDownload, FaExternalLinkAlt } from 'react-icons/fa';
 
+// Array of certificates with only local file paths
 const certificates = [
   {
     title: 'Web Development Bootcamp',
     issuer: 'Udemy',
     date: 'August 2023',
-    link: 'https://www.udemy.com/certificate/123456',
+    file: 'certificates/Backend/backend.pdf', // Local file path
   },
   {
     title: 'JavaScript Algorithms and Data Structures',
     issuer: 'freeCodeCamp',
     date: 'July 2023',
-    link: 'https://www.freecodecamp.org/certification/123456',
+    file: 'certificates/certificate2.pdf',
   },
   {
     title: 'React Developer Certification',
     issuer: 'Coursera',
     date: 'June 2023',
-    link: 'https://www.coursera.org/certificate/123456',
+    file: 'certificates/certificate3.pdf',
   },
   {
     title: 'Frontend Development with HTML, CSS, and JS',
     issuer: 'Codecademy',
     date: 'May 2023',
-    link: 'https://www.codecademy.com/certificate/123456',
+    file: 'certificates/certificate4.pdf',
   },
   {
     title: 'Python for Data Science',
     issuer: 'DataCamp',
     date: 'April 2023',
-    link: 'https://www.datacamp.com/certificate/123456',
+    file: 'certificates/certificate5.pdf',
   },
   {
     title: 'AI and Machine Learning Fundamentals',
     issuer: 'Stanford Online',
     date: 'March 2023',
-    link: 'https://online.stanford.edu/certificates/123456',
+    file: 'certificates/certificate6.pdf',
   },
 ];
 
@@ -55,8 +56,9 @@ const Certificates: React.FC = () => {
               <p className="text-gray-500">{certificate.issuer}</p>
               <p className="text-gray-600"><i className="fas fa-calendar-alt mr-2"></i>{certificate.date}</p>
               <div className="mt-4 flex justify-between items-center">
+                {/* View certificate (opens the local file in a new tab) */}
                 <a
-                  href={certificate.link}
+                  href={certificate.file} // Local file path from public folder
                   target="_blank"
                   rel="noopener noreferrer"
                   className="text-blue-600 hover:text-blue-800 flex items-center"
@@ -64,8 +66,10 @@ const Certificates: React.FC = () => {
                   <FaExternalLinkAlt className="mr-2" />
                   View Certificate
                 </a>
+
+                {/* Download certificate */}
                 <a
-                  href={certificate.link} // replace this with a direct download link if available
+                  href={certificate.file} // Local file path from public folder
                   download
                   className="text-green-600 hover:text-green-800 flex items-center"
                 >
