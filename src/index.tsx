9835b97843d435812407fa993377541a0ee97d5a
Uttam1910 <uttamt1202@gmail.com>
2026-08-09 15:53:49 +0530

    chore: update project configuration, add index entry point, define icon types, and upgrade dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import emailjs from 'emailjs-com';
-
-emailjs.init('YOUR_USER_ID');
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container!);
